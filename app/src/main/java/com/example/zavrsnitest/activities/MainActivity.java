f2e0607d6ab4433c6e0321b59dfb89cbcd8ea7f5
Adrijana <adrijana.jovicic@gmail.com>
2020-05-23 18:27:31 +0200

    toolbar and drawer - MainActivity

--- app/src/main/java/com/example/zavrsnitest/activities/MainActivity.java
+++ app/src/main/java/com/example/zavrsnitest/activities/MainActivity.java
@@ -1,16 +1,125 @@
 package com.example.zavrsnitest.activities;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.zavrsnitest.R;
+import com.example.zavrsnitest.settings.SettingsActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ArrayList<String> drawerItems;
+    private ListView drawerList;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private RelativeLayout drawerPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        setupToolbar();
+        fillDataDrawer();
+        setupDrawer();
     }
+
+    private void fillDataDrawer() {
+        drawerItems = new ArrayList<>();
+        drawerItems.add( "Lista filmova" );
+        drawerItems.add( "Settings" );
+        drawerItems.add( "O aplikaciji" );
+    }
+
+    private void setupDrawer() {
+        drawerList = findViewById( R.id.left_drawer );
+        drawerLayout = findViewById( R.id.drawer_layout );
+        drawerPane = findViewById( R.id.drawerPane );
+        drawerList.setAdapter( new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, drawerItems ) );
+
+        drawerList.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String title = "Unknown";
+                switch (i) {
+                    case 0:
+                        title = "Moji filmova";
+                        startActivity( new Intent( MainActivity.this, MojiFilmoviActivity.class ) );
+                        break;
+
+                    case 1:
+                        title = "Pretraga filmova";
+                        startActivity( new Intent( MainActivity.this, MainActivity.class ) );
+                        break;
+                    case 2:
+                        title = "Settings";
+                        Toast.makeText( getBaseContext(), "Prikaz podesavanja", Toast.LENGTH_SHORT );
+                        startActivity( new Intent( MainActivity.this, SettingsActivity.class ) );
+                        break;
+                    case 3:
+                        Toast.makeText( getBaseContext(), "Obisi sve filmove", Toast.LENGTH_SHORT );
+                        title = "Brisanje filmova";
+                        //TODO : brisanje cele liste filmova
+                        break;
+                    default:
+                        break;
+                }
+                setTitle( title );
+                drawerLayout.closeDrawer( Gravity.LEFT );
+            }
+        } );
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.app_name,
+                R.string.app_name
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle( "" );
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle( "" );
+                invalidateOptionsMenu();
+            }
+        };
+    }
+
+    public void setupToolbar() {
+        toolbar = findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+        toolbar.setTitleTextColor( Color.WHITE );
+        toolbar.setSubtitle( "Pretraga filmova" );
+        toolbar.setLogo( R.drawable.heart );
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled( true );
+            actionBar.setHomeAsUpIndicator( R.drawable.drawer );
+            actionBar.setHomeButtonEnabled( true );
+            actionBar.show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/zavrsnitest/activities/MojiFilmoviActivity.java
@@ -0,0 +1,16 @@
+package com.example.zavrsnitest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.zavrsnitest.R;
+
+public class MojiFilmoviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_moji_filmovi );
+    }
+}
